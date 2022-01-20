0c3e40e2205c44c01422f01fc7930e409d4e3fb4
Ashwin Vaidya <ashwinitinvaidya@gmail.com>
2022-01-20 14:35:15 +0100

    🛠  disable cudnn

--- external/anomaly/anomaly_classification/task.py
+++ external/anomaly/anomaly_classification/task.py
@@ -62,6 +62,10 @@ class AnomalyClassificationTask(ITrainingTask, IInferenceTask, IEvaluationTask, 
             task_environment (TaskEnvironment): OTE Task environment.
         """
         logger.info("Initializing the task environment.")
+
+        torch.backends.cudnn.enabled = False
+        logger.info(f"CUDNN status: {torch.backends.cudnn.enabled}")
+
         self.task_environment = task_environment
         self.model_name = task_environment.model_template.name
         self.labels = task_environment.get_labels()
