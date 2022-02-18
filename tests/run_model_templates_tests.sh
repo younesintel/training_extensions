63ddd0c61c08e82124d8cbe928a397c1aefa2bb2
Ilya Krylov <ilya.krylov@intel.com>
2022-02-18 13:48:52 +0300

    minor

--- tests/run_model_templates_tests.sh
+++ tests/run_model_templates_tests.sh
@@ -1,10 +1,10 @@
-# python3 -m venv venv || exit 1
+python3 -m venv venv || exit 1
 . venv/bin/activate || exit 1
-# pip install --upgrade pip || exit 1
-# pip install -e ote_cli || exit 1
-# pip install -e $OTE_SDK_PATH || exit 1
-# echo ""
-# echo ""
+pip install --upgrade pip || exit 1
+pip install -e ote_cli || exit 1
+pip install -e $OTE_SDK_PATH || exit 1
+echo ""
+echo ""
 
 export PYTHONPATH=${PYTHONPATH}:`pwd`
 pytest tests/ote_cli/*s_detection* -v --durations=0 || exit 1
